88d168b59f2f2919f08ba3f37eb221374f76fce5
Heather Christman <efl7a@yahoo.de>
2020-06-10 14:58:45 -0400

    fix typo

--- app/javascript/clickHandlers.js
+++ app/javascript/clickHandlers.js
@@ -3,7 +3,7 @@ import _ from "lodash";
 import renderDie from './renderDie';
 
 export const rollDie = () => {
-  for (let i = 1; i < 3; i++) {
+  for (let i = 0; i < 2; i++) {
     let number = _.random(1, 6);
     console.log(number);
     renderDie(number, i);
